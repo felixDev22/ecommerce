c8b080cf88382dcfa577ba0705c85fcef791d978
felixDev22 <felixnzomo@gmail.com>
2024-09-16 22:11:09 +0300

    Update the logo, and food item 7

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,10 @@
-import logo from './logo.jpg';
+import logo from './logo.png';
 import food1 from './food-1.jpg';
 import food2 from './food-2.jpg';
 import food3 from './food-3.jpg';
 import food4 from './food-4.jpg';
 import food5 from './food-5.jpg';
 import food6 from './food-6.jpg';
-import food7 from './food-7.jpg';
+import food7 from './food-7.png';
 
 export const assets = { logo, food1, food2, food3, food4, food5, food6, food7 };
