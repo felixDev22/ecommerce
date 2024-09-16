5231ae2a9a69d2d9adcfab4e34d37504d5214cb3
felixDev22 <felixnzomo@gmail.com>
2024-09-16 22:12:43 +0300

    Create Navbar section and nav style file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
-import './Navbar.css'
-
+import './Navbar.css';
+import {assets} from '../../assets/index';
 
 const Navbar = () => {
-  return(
-     <div>
-      <h2>This is Navbar</h2>
-  </div>
-  )
+  return (
+    <div className="nav">
+      <img src={assets.logo} alt="logo" className="logo" />
+     <ul className="nav-menu">
+     <li>
+
+     </li>
+     </ul>
+
+    
+    </div>
+  );
 };
 
 export default Navbar;
