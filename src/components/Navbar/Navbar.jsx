54fb5c421d2cd6f0f28972327220f24a852e9e9e
felixDev22 <felixnzomo@gmail.com>
2024-09-17 13:45:18 +0300

    Import the search, cart icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import './Navbar.css';
-import {assets} from '../../assets/index';
+import { assets } from '../../assets/index';
+import { icons } from '../../assets/index';
 
 const Navbar = () => {
   return (
     <div className="nav">
       <img src={assets.logo} alt="logo" className="logo" />
-     <ul className="nav-menu">
-     <li>
-
-     </li>
-     </ul>
-
-    
+      <ul className="nav-menu">
+        <li>menu</li>
+        <li>download app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="nav-right">
+        <icons.FaMagnifyingGlass className="nav-icon" />
+        <div>
+          <icons.FaCartPlus className="nav-icon" />
+        </div>
+      </div>
     </div>
   );
 };
