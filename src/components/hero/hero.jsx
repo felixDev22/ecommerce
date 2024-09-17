79b54f5b993064b7184f93fd2478b13d787bd3ca
felixDev22 <felixnzomo@gmail.com>
2024-09-17 22:15:22 +0300

    Create hero component and css file

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,11 @@
+import { assets } from '../../assets/index';
+
+const Hero = () => {
+  return (
+    <div>
+      <img src={assets.banner} alt="banner" />
+    </div>
+  );
+};
+
+export default Hero;
