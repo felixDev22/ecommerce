17b87da3a384fe042ddc71b3b49d9a4aed536f02
felixDev22 <felixnzomo@gmail.com>
2024-09-17 22:14:12 +0300

    Create benner for hero, add to assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,9 +6,11 @@ import food4 from './food-4.jpg';
 import food5 from './food-5.jpg';
 import food6 from './food-6.jpg';
 import food7 from './food-7.png';
+import banner from './pizza-header.jpg';
+
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 import { FaCartPlus } from 'react-icons/fa6';
 
 
-export const assets = { logo, food1, food2, food3, food4, food5, food6, food7 };
+export const assets = { logo, food1, food2, food3, food4, food5, food6, food7, banner };
 export const icons = { FaMagnifyingGlass, FaCartPlus };
