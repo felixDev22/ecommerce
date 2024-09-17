301c4e4e025f7f7fa78242da976c2ae4a0d0fe5a
felixDev22 <felixnzomo@gmail.com>
2024-09-17 17:04:21 +0300

    Update nav li and add button for login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,9 +7,10 @@ const Navbar = () => {
     <div className="nav">
       <img src={assets.logo} alt="logo" className="logo" />
       <ul className="nav-menu">
-        <li>menu</li>
-        <li>download app</li>
-        <li>contact us</li>
+        <li>Menu</li>
+        <li>Whats new?</li>
+        <li>Download app</li>
+        <li>Contact us</li>
       </ul>
       <div className="nav-right">
         <icons.FaMagnifyingGlass className="nav-icon" />
@@ -17,6 +18,7 @@ const Navbar = () => {
           <icons.FaCartPlus className="nav-icon" />
         </div>
       </div>
+      <button className="login-btn">Log-in</button>
     </div>
   );
 };
