7c44fa69574b776c373d966e636bdc01be0a493d
felixDev22 <felixnzomo@gmail.com>
2024-09-20 23:50:57 +0300

    Export and Import the menu section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Hero from './components/hero/hero';
 import Navbar from './components/Navbar/Navbar';
+import MenuSection from './components/menuSection/MenuSection';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Navbar />
         <Hero />
+        <MenuSection />
       </div>
     </>
   );
