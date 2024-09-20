0e31cd2bbe9cbec6f1a7a9f446707fce0c20fae0
felixDev22 <felixnzomo@gmail.com>
2024-09-21 00:53:36 +0300

    Export the images to menusection component

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,9 +8,67 @@ import food6 from './food-6.jpg';
 import food7 from './food-7.png';
 import banner from './pizza-header.jpg';
 
+import cat01 from './menu-cat1.png';
+import cat02 from './menu-cat2.png';
+import cat03 from './menu-cat3.png';
+import cat04 from './menu-cat4.png';
+import cat05 from './menu-cat5.png';
+import cat06 from './menu-cat6.png';
+import cat07 from './menu-cat7.png';
+import cat08 from './menu-cat8.png';
+import cat09 from './menu-cat9.png';
+
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 import { FaCartPlus } from 'react-icons/fa6';
 
-
-export const assets = { logo, food1, food2, food3, food4, food5, food6, food7, banner };
+export const assets = {
+  logo,
+  food1,
+  food2,
+  food3,
+  food4,
+  food5,
+  food6,
+  food7,
+  banner,
+};
 export const icons = { FaMagnifyingGlass, FaCartPlus };
+
+export const menuCategory = [
+  {
+    name: 'Soft-Drinks',
+    image: cat01,
+  },
+  {
+    name: 'Desserts',
+    image: cat02,
+  },
+  {
+    name: 'Pizza',
+    image: cat03,
+  },
+  {
+    name: 'Burgers',
+    image: cat04,
+  },
+  {
+    name: 'Salad',
+    image: cat05,
+  },
+  {
+    name: 'Coffee',
+    image: cat06,
+  },
+  {
+    name: 'Pastry',
+    image: cat07,
+  },
+  {
+    name: 'Swahili Dishes',
+    image: cat08,
+  },
+  {
+    name: 'Meals',
+    image: cat09,
+  },
+];
