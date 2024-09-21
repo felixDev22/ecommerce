8939074d54769b8a1b0cd80c963593ae03c42d8a
felixDev22 <felixnzomo@gmail.com>
2024-09-21 17:55:09 +0300

    Use map method to populate the menus

--- src/components/menuSection/menuSection.jsx
+++ src/components/menuSection/menuSection.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { menuCategory } from '../../assets';
+import './menuSection.css';
 
 const MenuSection = () => {
   return (
-    <div>MenuSection</div>
-  )
-}
+    <div className="menu-section">
+      <div>
+        <h1>Choose Your Favorite Dish</h1>
+        <p>
+          Scroll to explore a variety of mouthwatering meals waiting for you
+        </p>
+      </div>
+      <div className="menu-list">
+        {menuCategory.map((item, index) => {
+          return (
+            <div className="menu-items" key={index}>
+              <img src={item.image} alt={item.name} className="menu-img" />
+              <p>{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default MenuSection
+export default MenuSection;
