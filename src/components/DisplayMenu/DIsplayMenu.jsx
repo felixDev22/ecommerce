16d5a19bc07f17d3d0875fdd58b88efaca42ea33
felixDev22 <felixnzomo@gmail.com>
2024-09-29 01:13:43 +0300

    Pass props, populate the menu using map method

--- src/components/DisplayMenu/DIsplayMenu.jsx
+++ src/components/DisplayMenu/DIsplayMenu.jsx
@@ -1,13 +1,28 @@
 import { useContext } from 'react';
 import { StoreContext } from '../../context/Store';
 
-const DisplayMenu = () => {
-
+const DisplayMenu = ({ category }) => {
   const { menuItems } = useContext(StoreContext);
 
   return (
     <div className="menu-display" id="menu-display">
       <h2>Make your choose, click and make your order</h2>
+      <div>
+        {menuItems.map((item, index) => {
+          return (
+            <div key={index} className="menu-wrapper">
+              <div>
+                <img src={item.image} className="menu-img" />
+              </div>
+              <div className="menu-info">
+                <h4>{item.type}</h4>
+                <p>{item.desc}</p>
+                <h5>{item.price}</h5>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
