2beae3c86f89306fa35fb6202f2778ca2597940d
felixDev22 <felixnzomo@gmail.com>
2024-10-07 17:03:55 +0300

    Add function for add and remove to cart

--- src/context/Store.jsx
+++ src/context/Store.jsx
@@ -1,11 +1,29 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import { menuItems } from '../assets';
 
 export const StoreContext = createContext(null);
 
 const StoreProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addCartItems = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeCartItems = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
   const contextValue = {
     menuItems,
+    cartItems,
+    setCartItems,
+    addCartItems,
+    removeCartItems,
   };
 
   return (
