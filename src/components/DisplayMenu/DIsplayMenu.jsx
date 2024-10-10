be9059dc478569164a7a415f3f95cbd35903e18c
felixDev22 <felixnzomo@gmail.com>
2024-10-10 23:14:45 +0300

    Import Proptypes and an if condition for the category

--- src/components/DisplayMenu/DIsplayMenu.jsx
+++ src/components/DisplayMenu/DIsplayMenu.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { StoreContext } from '../../context/Store';
 import './displayMenu.css';
@@ -12,27 +13,32 @@ const DisplayMenu = ({ category }) => {
       <h2>Make your choice, click and make your order</h2>
       <div className="menuDisplay-container">
         {menuItems.map((item, index) => {
-          return (
-            <div key={index} className="menuDisplay-card">
-              <img
-                src={item.image}
-                className="menuDisplay-img"
-                alt={item.type}
-              />
-
-              <div className="menu-info">
-                <Rating />
-                <h4>{item.type}</h4>
-                <p>{item.disc}</p>
-                <h5>Ksh {item.price}</h5>
-                <AddItems id={item.id} />
+          if (category === 'ALL' || category === item.category) {
+            return (
+              <div key={index} className="menuDisplay-card">
+                <img
+                  src={item.image}
+                  className="menuDisplay-img"
+                  alt={item.type}
+                />
+                <div className="menu-info">
+                  <Rating />
+                  <h4>{item.type}</h4>
+                  <p>{item.disc}</p>
+                  <h5>Ksh {item.price}</h5>
+                  <AddItems id={item.id} />
+                </div>
               </div>
-            </div>
-          );
+            );
+          }
         })}
       </div>
     </div>
   );
+};
+
+DisplayMenu.propType = {
+  category: PropTypes.string.isRequired,
 };
 
 export default DisplayMenu;
