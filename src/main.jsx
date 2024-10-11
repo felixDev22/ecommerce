34e67b45a2eb6a24c70376951c449207f7009c18
felixDev22 <felixnzomo@gmail.com>
2024-10-11 23:55:28 +0300

    Wrap the app with browsers router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import StoreProvider from './context/Store';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <StoreProvider>
       <App />
     </StoreProvider>
-  </StrictMode>,
+  </BrowserRouter>,
 );
