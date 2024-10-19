464d971cfb556044b2cf17e062d5c4ad10d1ffcb
felixDev22 <felixnzomo@gmail.com>
2024-10-19 23:36:50 +0300

    Destructure the setShowlogin, add onclick event

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,9 +1,15 @@
-const Login = () => {
+import { useState } from 'react';
+import { IoClose } from 'react-icons/io5';
+
+const Login = ({setShowLogin}) => {
+  const [curr, setCurr] = useState('Sign-up');
+
   return (
     <div className="Login">
       <form className="login-wrapper">
         <div className="login-title">
-          <h2>Pop-up Login</h2>
+          <h2>{curr}</h2>
+          <IoClose onClick={setShowLogin} />
         </div>
       </form>
     </div>
