9322d2a8319d83d9417d3ea352f366907c0614f3
felixDev22 <felixnzomo@gmail.com>
2024-10-19 22:49:53 +0300

    Create the login component

---
+++ src/components/login/login.jsx
@@ -0,0 +1,13 @@
+const login = () => {
+  return (
+    <div className="Login">
+      <form className="login-wrapper">
+        <div className="login-title">
+          <h2>Pop-up Login</h2>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default login;
