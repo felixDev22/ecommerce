36b6e5da105d79bfaf0c9a46f0463ca23d8fe966
felixDev22 <felixnzomo@gmail.com>
2024-10-20 00:28:19 +0300

    Update the login props and onclick func

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { IoClose } from 'react-icons/io5';
+import PropTypes from 'prop-types';
 
-const Login = ({setShowLogin}) => {
+const Login = ({ setShowLogin }) => {
   const [curr, setCurr] = useState('Sign-up');
 
   return (
@@ -9,11 +10,14 @@ const Login = ({setShowLogin}) => {
       <form className="login-wrapper">
         <div className="login-title">
           <h2>{curr}</h2>
-          <IoClose onClick={setShowLogin} />
+          <IoClose onClick={()=>setShowLogin(false)} />
         </div>
       </form>
     </div>
   );
+};
+Login.propTypes = {
+  setShowLogin: PropTypes.func.isRequired,
 };
 
 export default Login;
