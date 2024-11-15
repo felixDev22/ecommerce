501bd336ad77900fd2bba2d3b0545a79dc156072
felixDev22 <felixnzomo@gmail.com>
2024-11-15 08:24:02 +0300

    Set up the cart logic

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,12 +1,28 @@
 import { useContext } from 'react';
 import { StoreContext } from '../../context/Store';
+import './cart.css';
 
 const Cart = () => {
   const { menuItems, cartItems, removeCartItems } = useContext(StoreContext);
 
   return (
     <div className="cart-container" id="cart">
-      <h1>This is Cart page</h1>
+      <div className="cart-display">
+        <h1>This is the Cart page</h1>
+
+        {menuItems.map((item, index) =>
+          cartItems[item.id] > 0 ? (
+            <div key={index} className="cart-items">
+              <img src={item.image} alt={item.name} />
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+              <p>{cartItems[item.id]}</p>
+              <p>{item.price * cartItems[item.id]}</p>
+              <button onClick={() => removeCartItems(item.id)}>Remove</button>
+            </div>
+          ) : null,
+        )}
+      </div>
     </div>
   );
 };
