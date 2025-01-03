17beaca968d9ca21a414aec0dd47b06c08574457
felixDev22 <felixnzomo@gmail.com>
2025-01-03 21:12:48 +0300

    Add state to manage mobile menu and update style menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/index';
 import { icons } from '../../assets/index';
@@ -5,17 +6,22 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ setShowLogin }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className="nav">
       <Link to="/">
         <img src={assets.logo} alt="logo" className="logo" />
       </Link>
-      <ul className="nav-menu">
+      <ul className={`nav-menu ${menuOpen ? 'show' : ''}`}>
         <a href="#menu">Menu</a>
-        <a href="#">Whats new?</a>
-        <a href="#">Download app</a>
         <a href="#footer">Contact us</a>
       </ul>
+
       <div className="nav-right">
         <icons.FaMagnifyingGlass className="nav-icon" />
         <Link to="cart">
@@ -27,6 +33,9 @@ const Navbar = ({ setShowLogin }) => {
       <button className="login-btn" onClick={() => setShowLogin(true)}>
         Sign-up
       </button>
+      <div onClick={toggleMenu} className="menu-icon">
+        <icons.IoMenu className="menu" />
+      </div>
     </div>
   );
 };
